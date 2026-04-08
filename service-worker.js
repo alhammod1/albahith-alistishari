bbf1fe220e6c0acc01b0c3447213e03728ddd4b6
alhammod <yahoo_sa@hotmail.com>
2026-04-08 21:43:36 +0300

    Add files via upload
    first upload

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'albahith-cache-v2';
+const ASSETS = ['./','./index.html','./manifest.webmanifest','./service-worker.js'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
